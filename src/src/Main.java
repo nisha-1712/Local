12463887df252123eda0f9e342dc599ee5e59330
Nisha Bhavsar <101039209+nisha-1712@users.noreply.github.com>
2022-03-27 14:51:48 +0530

    Block Concept

---
+++ src/src/Main.java
@@ -0,0 +1,23 @@
+package src;
+
+public class Main {
+    int a, b;
+
+    {
+        a = 100;
+        b = 200;
+        System.out.println(a + " " + b);
+    }
+
+    {
+        a = 80;
+        b = 90;
+        System.out.println(a + " " + b);
+    }
+
+}
+class Blocks{
+        public static void main(String[] args) {
+	        Main obj =new Main();
+    }
+}
